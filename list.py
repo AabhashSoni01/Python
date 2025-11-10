b68802001c15bee21210acb2c981a432d6cda9c4
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-11-10 22:28:17 +0530

    Nov-10

---
+++ list.py
@@ -0,0 +1,13 @@
+# list = [1, 2, 3, 4, 5]
+# sum = 0
+# for item in list:
+#     sum += item
+# print(sum)
+
+# list = [0, 1, 0, 3, 12, 0, 5]
+# left = 0
+# for right in range(len(list)):
+#     if list[right] != 0:
+#         list[left], list[right] = list[right], list[left]
+#         left += 1
+# print(list)
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-Prints the first n rows of Pascal's Triangle.
-# Args: n (int): The number of rows to print.
