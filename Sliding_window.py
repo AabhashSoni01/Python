117c72bf7326a54985e6231c7198015a2be3417e
AabhashSoni01 <aabhashsoni2@gmail.com>
2025-11-13 23:40:19 +0530

    Sliding window

---
+++ Sliding_window.py
@@ -0,0 +1,17 @@
+def max_sum_subarray(arr, k):
+    n = len(arr)
+    if n < k:
+        return "Invalid input"
+
+    window_sum = sum(arr[:k])
+    max_sum = window_sum
+
+    for i in range(k, n):
+        window_sum += arr[i] - arr[i - k]
+        max_sum = max(max_sum, window_sum)
+
+    return max_sum
+
+arr = [2, 1, 5, 1, 3, 2]
+k = 3
+print("Maximum sum of subarray of size", k, "=", max_sum_subarray(arr, k))
